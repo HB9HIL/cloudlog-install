ec7c0c3cf260dea7d5ecdb5092cc93ed6fa8b62c
HB9HIL <80885850+HB9HIL@users.noreply.github.com>
2023-09-13 09:41:35 +0200

    work in progress

--- cloudlog-install.sh
+++ cloudlog-install.sh
@@ -164,7 +164,7 @@ sudo mysql -u root -e "FLUSH PRIVILEGES"
 
 # Prepare the Webroot Folder
 sudo mkdir -p $INSTALL_PATH
-sudo git clone -b dev https://github.com/magicbug/Cloudlog.git $INSTALL_PATH
+sudo git clone https://github.com/magicbug/Cloudlog.git $INSTALL_PATH
 
 # Set the Permissions
 
@@ -196,122 +196,9 @@ sed -i "s/define('ENVIRONMENT', 'development');/define('ENVIRONMENT', 'productio
 sudo a2ensite cloudlog.conf
 sudo systemctl restart apache2
 
-##################################################################################################################################
-#######                                                                                                                    #######
-clear && exit 1 ####################################     EDITED UNTIL HERE     ###################################################
-#######                                                                                                                    #######
-##################################################################################################################################
+clear
 
-# End Message
-FIELD_W=30
-ENTRY_W=60
+final_message_dimensions=$(calculating_box "$DEFINED_LANG/final_message.txt")
+dialog --title "SQL Setup" --msgbox "$(cat $DEFINED_LANG/final_message.txt)" $final_message_dimensions
 
-echo ""
-echo ""
-echo ""
-echo "_____________________________________________________________________________________________________"
-echo ""
-echo ""
-echo ""
-
-if [[ $language =~ ^($CHOOSELANG)$ ]]; then
-    echo ">>> The script is now complete."
-    echo ""
-    echo "Access the following address with your web browser: http://$LOCAL_IP/install"
-    echo ""
-    echo "!!! Write down the DB_PASSWORD at a safe place !!!"
-    echo ""
-    echo "Now enter the following data in the installer. Note that any reverse proxy must be configured BEFORE clicking 'Install':"
-    echo ""
-    # Function to create a row in the table
-    print_table_row() {
-            local field_width=$((FIELD_W - 1))
-            local entry_width=$((ENTRY_W - 1))
-            local field=$1
-            local entry=$2
-
-            printf "| %-${field_width}s | %-${entry_width}s |\n" "$field" "$entry"
-    }
-
-    # output of the table
-    echo "+$(printf -- '-%.0s' $(seq $((FIELD_W+1))))+$(printf -- '-%.0s' $(seq $((ENTRY_W+1))))+" 
-    echo "| $(printf -- '%-*s' $((FIELD_W-1)) "Feld") | $(printf -- '%-*s' $((ENTRY_W-1)) "Eintrag") |"
-    echo "+$(printf -- '-%.0s' $(seq $((FIELD_W+1))))+$(printf -- '-%.0s' $(seq $((ENTRY_W+1))))+"
-
-    print_table_row "DIRECTORY" "left empty or '$INSTALL_PATH'"
-    print_table_row "Webseite URL" "planned URL to reach Cloudlog"
-    print_table_row "Default Gridsquare" "your local Maidenhead-Locator"
-    print_table_row "hostname" "localhost"
-    print_table_row "Username" "$DB_USER"
-    print_table_row "Password" "$DB_PASSWORD"
-    print_table_row "Database Name" "$DB_NAME"
-
-    echo "+$(printf -- '-%.0s' $(seq -s ' ' $((FIELD_W+1))))+$(printf -- '-%.0s' $(seq -s ' ' $((ENTRY_W+1))))+"""
-    echo ""
-    echo "Afterwards, click on 'Install'."
-    echo "Congratulations. You can now log in at http://$LOCAL_IP/ or the URL you have set in the 'Website URL' field with the demo credentials."
-    echo "When you have completed the install wizard, do the following:"
-    echo "    - Log in with username m0abc and password demo"
-    echo "    - Create a new admin account (Admin Dropdown) and delete the demo account"
-    echo "    - Update Country Files (Admin Dropdown)"
-    echo "    - Create a station profile (Admin Dropdown) and set it as active"
-    echo "    - If you want to know if the person you're working uses LoTW, run: http://$LOCAL_IP/index.php/lotw/load_users. This is the initial run, but we'll run this every week from cron momentarily."
-    echo ""
-    echo "Thank you for using this script. If you encounter any issues, please contact support@hb9hil.org for assistance."
-    echo "The MySQL (MariaDB) installation will now be secured using 'sudo mysql_secure_installation' as a last step"
-    echo ""
-    echo ""
-    read -r -p "Press Enter to continue..."
-    sudo mysql_secure_installation
-
-else
-    echo ">>> Das Script ist nun fertig."
-    echo ""
-    echo "Rufen Sie mit Ihrem Webbrowser folgende Adresse auf: http://$LOCAL_IP/install"
-    echo ""
-    echo "!!! Notieren Sie sich das DB_PASSWORD an einem sicheren Ort !!!"
-    echo ""
-    echo "In dem Installer geben Sie nun folgende Daten ein. Beachten Sie, dass ein allfälliger Reverse Proxy konfiguriert werden muss BEVOR man auf 'Install' klickt:"
-    echo ""
-    # Funktion zum Erstellen einer Zeile in der Tabelle
-    print_table_row() {
-            local field_width=$((FIELD_W - 1))
-            local entry_width=$((ENTRY_W - 1))
-            local field=$1
-            local entry=$2
-
-            printf "| %-${field_width}s | %-${entry_width}s |\n" "$field" "$entry"
-    }
-
-    # Ausgabe der Tabelle
-    echo "+$(printf -- '-%.0s' $(seq $((FIELD_W+1))))+$(printf -- '-%.0s' $(seq $((ENTRY_W+1))))+"
-    echo "| $(printf -- '%-*s' $((FIELD_W-1)) "Feld") | $(printf -- '%-*s' $((ENTRY_W-1)) "Eintrag") |"
-    echo "+$(printf -- '-%.0s' $(seq $((FIELD_W+1))))+$(printf -- '-%.0s' $(seq $((ENTRY_W+1))))+"
-
-    print_table_row "DIRECTORY" "leer lassen oder '$INSTALL_PATH'"
-    print_table_row "Webseite URL" "geplante URL wie Cloudlog erreicht werden soll"
-    print_table_row "Default Gridsquare" "Dein lokaler Maidenhead-Locator"
-    print_table_row "hostname" "localhost"
-    print_table_row "Username" "$DB_USER"
-    print_table_row "Password" "$DB_PASSWORD"
-    print_table_row "Database Name" "$DB_NAME"
-
-    echo "+$(printf -- '-%.0s' $(seq -s ' ' $((FIELD_W+1))))+$(printf -- '-%.0s' $(seq -s ' ' $((ENTRY_W+1))))+"
-
-    echo ""
-    echo "Klicken Sie anschließend auf 'Installieren'."
-    echo "Herzlichen Glückwunsch. Sie können sich jetzt unter http://$LOCAL_IP/ oder der URL, die Sie im Feld 'Website URL' festgelegt haben, mit den Demo-Anmeldedaten anmelden."
-    echo "Wenn Sie den Installationsassistenten abgeschlossen haben, führen Sie bitte folgende Schritte aus:"
-    echo "    - Melden Sie sich mit Benutzername m0abc und Passwort demo an"
-    echo "    - Erstellen Sie ein neues Administrator-Konto (Administrator-Dropdown) und löschen Sie das Demo-Konto"
-    echo "    - Aktualisieren Sie Länderdateien (Administrator-Dropdown)"
-    echo "    - Erstellen Sie ein Stationsprofil (Administrator-Dropdown) und setzen Sie es als aktiv"
-    echo "    - Wenn Sie wissen möchten, ob die Person, mit der Sie arbeiten, LoTW verwendet, führen Sie Folgendes aus: http://$LOCAL_IP/index.php/lotw/load_users. Dies ist der erste Durchlauf, aber wir werden dies wöchentlich aus dem Cron ausführen."
-    echo ""
-    echo "Vielen Dank, dass Sie dieses Skript verwenden. Wenn Sie auf Probleme stoßen, wenden Sie sich bitte an support@hb9hil.org, um Unterstützung zu erhalten."
-    echo "Die MySQL (MariaDB)-Installation wird nun als letzter Schritt noch mit 'sudo mysql_secure_installation' abgesichert."
-    echo ""
-    echo ""
-    read -r -p "Drücken Sie die Eingabetaste, um fortzufahren..."
-    sudo mysql_secure_installation
-fi
+sudo mysql_secure_installation
