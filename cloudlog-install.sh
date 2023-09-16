2115c08d47402bfe468179d6c6428eb91020bc3d
HB9HIL <80885850+HB9HIL@users.noreply.github.com>
2023-09-16 09:10:27 +0200

    adjust errorstop function

--- cloudlog-install.sh
+++ cloudlog-install.sh
@@ -4,6 +4,7 @@
 # shellcheck disable=SC2086
 # shellcheck disable=SC2024
 # shellcheck disable=SC2005
+# shellcheck disable=SC2120
 # shellcheck source=/dev/null
 
 # Variables
@@ -41,7 +42,10 @@ errorstop() {
     clear 
     echo "Uuups... Something went wrong here, Try to start the script again."
     echo "Please create an issue at https://github.com/HB9HIL/cloudlog-install/issues"
-    echo "!!! ERRORSTOP" >> $LOG_FILE
+    echo "!!! ERRORSTOP !!!" >> $LOG_FILE
+    local error_message="$1"
+    echo "The script run into an error: $error_message" >&2 
+    echo "The script run into an error: $error_message" >> "$LOG_FILE"
     read -r -p "Press Enter to stop the script. Restart it manually."
     exit 1
 }
